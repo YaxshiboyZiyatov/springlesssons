5248d9635c839d6b3855f246745067a575ecf017
YaxshiboyZiyatov <ziyatovyaxshiboy114@gmail.com>
2021-05-29 11:01:16 +0300

    firstCommit

---
+++ src/main/java/com/example/springlesssons/SpringlesssonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlesssons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlesssonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlesssonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlesssons/SpringlesssonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlesssons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlesssonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
